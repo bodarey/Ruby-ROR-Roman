fd445a3036426e8cfd7dfeee4012384148814d0c
bodarey <69037258+bodarey@users.noreply.github.com>
2024-08-09 19:14:25 +0100

    refactoring

--- 18/app.rb
+++ 18/app.rb
@@ -1,6 +1,29 @@
-f = File.open('file.txt','r+')
-while (line = f.gets) do
-  puts line
-
+f = File.open('file.txt', 'r+')
+output = File.open('output.txt', 'w')
+while (line = f.gets)
+  condition = line.strip.length == 6
+  puts line if condition
+  output.puts line if condition
 
 end
+
+def is_password_weak?(password)
+  password.strip!
+  f = File.open('file.txt', 'r')
+  while (line = f.gets)
+    line.strip!
+    if password.include? line
+      answer = 'weak'
+      break
+    else
+      answer = 'your password is not weak'
+    end
+
+  end
+  f.close
+  answer == 'weak'
+end
+
+puts 'enter your password'
+password = gets
+puts is_password_weak? password
